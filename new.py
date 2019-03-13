0a6cadfe4346fac90bc1d74df3a9f6a6fc93903e
anzunming <2190881697@qq.com>
2019-03-13 14:17:55 +0800

    Create new.py

---
+++ new.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+#coding:utf-8
+import random
+number = int(random.uniform(1,10)
+key=input(unicode('输入数字:','utf-8').encode('gbk'))
+if key==number
+	print u"太厉害了，第一次就对了"
+while(key != number)
+	if key < number
+		key=int(intput( u"太小了,再输入:"))
+	if key > number
+		key=int(intput( u"太大了,再输入:"))
+	if key == number
+		print u"太厉害了，猜对了"
+		break;
