d0e1cbfa47542ddf724103a5939965a90c56684a
anzunming <2190881697@qq.com>
2019-03-13 14:17:47 +0800

    Create if_else.py

---
+++ if_else.py
@@ -0,0 +1,13 @@
+#!usr/bin/evn python
+#coding:utf-8
+num = 10
+if num == 1:
+	print 'apple'
+elif num == 2:
+	print 'iphone'
+elif num == 3:
+	print 'ipad'
+elif num < 0:
+	print 'orange'
+else:
+	print 'other'
